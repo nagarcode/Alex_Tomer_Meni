e5a2638684aac98b3ca52013eae1ab93bddc2252
alexdegti <85748958+alexdegti@users.noreply.github.com>
2021-12-07 20:09:44 +0200

    Update RegisterWithPreferences.java

--- code/src/main/java/bgu/atd/a1/sim/actions/RegisterWithPreferences.java
+++ code/src/main/java/bgu/atd/a1/sim/actions/RegisterWithPreferences.java
@@ -1,16 +1,68 @@
 package bgu.atd.a1.sim.actions;
 
 import bgu.atd.a1.Action;
+import bgu.atd.a1.sim.actions.ParticipatingInCourse;
+import bgu.atd.a1.sim.privateStates.CoursePrivateState;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.concurrent.CountDownLatch;
+
+import java.lang.InterruptedException;
 
 public class RegisterWithPreferences extends Action<Void>{
 
-	public RegisterWithPreferences(){
+	private String studentID;
+	private List<String> preferences;
+	private List<Integer> grades; 
+
+	public RegisterWithPreferences(String studentID, List<String> preferences, List<Integer> grades){
+
+		this.studentID = studentID;
+		this.preferences = preferences;
+		this.grades = grades;
+
+		setActionName("Register With Preferences");
 
 	}
 
 	@Override
 	protected void start(){
-		System.out.println("For pet's sake!!!");
+
+		int i = -1;
+
+		for(String preference : preferences){
+
+			if((getResult()).isResolved())
+				break;
+
+			i += 1;
+			ParticipatingInCourse participatingInCourse = new ParticipatingInCourse(studentID, preference, grades.get(i));
+			List<ParticipatingInCourse> dependencies = new LinkedList<>();
+			dependencies.add(participatingInCourse);
+
+			CountDownLatch countDownLatch = new CountDownLatch(1);
+
+			then(dependencies, () -> {
+				if((participatingInCourse.getResult()).get())
+					complete(null);
+				countDownLatch.countDown();
+			});
+
+			sendMessage(participatingInCourse, preference, new CoursePrivateState());
+
+			try{
+				countDownLatch.await();
+			}
+			catch(InterruptedException exception){
+				System.out.println("Thread was interrupted.");
+			}	
+
+		}
+
+		if(!(getResult()).isResolved())
+			complete(null);
+
 	}
 	
 }
